fc95d7f13ca79a91016167ffcecf91084e54528c
Abram Sumilat <asumilat1027@gmail.com>
2026-02-08 21:18:51 -0500

    Accounts app setup + add gitignore

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ("username", "email", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    # Fields shown when EDITING an existing user
+    fieldsets = UserAdmin.fieldsets
+
+    # Fields shown when CREATING a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+
+    search_fields = ("username", "email")
+    ordering = ("username",)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        RECRUITER = "RECRUITER", "Recruiter"
+        JOBSEEKER = "JOBSEEKER", "Job seeker"
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.JOBSEEKER)
+
+    def __str__(self):
+        return self.username
+
+class RecruiterProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=255)
+
+    def clean(self):
+        if hasattr(self.user, "jobseekerprofile"):
+            raise ValidationError("User already has a JobSeekerProfile.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username} (Recruiter)"
+
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    major = models.CharField(max_length=120, blank=True)
+
+    def clean(self):
+        if hasattr(self.user, "recruiterprofile"):
+            raise ValidationError("User already has a RecruiterProfile.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username} (Job Seeker)"
