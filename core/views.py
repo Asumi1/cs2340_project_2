c69d340cbe3ec911d881463561ec514e16b19156
AAsteriskz7 <avsadarsh7@gmail.com>
2026-02-16 08:55:02 -0500

    fixed job seeker landing on landing page instead of dashboard

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect("admin_dashboard")
+        elif request.user.role == "RECRUITER":
+            return redirect("recruiter_dashboard")
+        else:
+            return redirect("jobseeker_dashboard")
     return render(request, "core/HomePage.html")
